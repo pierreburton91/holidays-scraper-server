32ebc08f6b221d41f9ddb0182e7c986f41d62745
pierreburton91 <pierre.burton91@gmail.com>
2018-07-26 15:26:11 +0200

    Basic travelbird scrape works !

--- index.js
+++ index.js
@@ -9,48 +9,56 @@ app.get('/', function (req, res) {
 })
 
 app.get('/search', async function (req, res) {
-        
+    req.setTimeout(0);
     const budget = req.query.budget 
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({slowMo: 10}) // SloMo to wait for content load
     const page = await browser.newPage()
     let isNextPageAvailable
     let offersThatMatch = []
+    await page.setViewport({width: 1920, height: 1080}) // Best result as desktop viewport
     await page.goto('https://fr.travelbird.be/search')
 
     do {
-        console.log('New page request')
-        const offersSelector = '#js-search-grid-cards > ul > li.grid-cards__item:not(grid-cards__item--break)'
-        const budgetSelector = '#js-search-grid-cards > ul > li.grid-cards__item:not(grid-cards__item--break):nth-child(INDEX) > a > div.offer-card__info > div > span.offer-card__price-value'
-        const linkToNextPage = '#js-search-result > div > a:last-child'
+        const offersSelector = '#js-search-grid-cards > ul > li'
+        const budgetSelector = '#js-search-grid-cards > ul > li:nth-child(INDEX) > a > div.offer-card__info > div > span.offer-card__price-value'
+        const linkToNextPageSelector = '#js-search-result > div > .pagination-btn:last-child'
 
-        let resultsLength = await page.evaluate((sel) => {
+        let offersLength = await page.evaluate((sel) => {
             const length = document.querySelectorAll(sel).length
             return length
         }, offersSelector)
 
-        for (i = 1; i <= resultsLength; i++) {
-            let indexedBudgetSelector = budgetSelector.replace('INDEX', i)
-            if (i != 7) {
+        for (i = 1; i <= offersLength; i++) {
+            let offerPrice = budgetSelector.replace('INDEX', i)
+            try { // Avoid error on ads
                 let result = await page.evaluate((sel) => {
                     const value = parseFloat(document.querySelector(sel).innerHTML.replace('.', '').replace(' €', '').replace(',', '.'))
                     return value
-                }, indexedBudgetSelector)
+                }, offerPrice)
                 if (result <= budget) {
                     offersThatMatch.push(result)
                 }
+            } catch (error) {
+                console.log(error)
+                continue
             }
         }
-        console.log('Offers : ' + offersThatMatch)
+
+        await page.waitFor(linkToNextPageSelector, {visible: true})
         isNextPageAvailable = await page.evaluate((sel) => {
             const isDisabled = document.querySelector(sel).classList.contains('disabled')
             return !isDisabled
-        }, linkToNextPage)
-        
+        }, linkToNextPageSelector)
+
         if (isNextPageAvailable) {
-            await page.click(linkToNextPage);
-            await page.waitForNavigation();
-            console.log('Navigated to new page')
+            await page.hover(linkToNextPageSelector)
+            await page.click(linkToNextPageSelector)
+            await page.waitForNavigation({
+                waitUntil: 'networkidle2',
+                timeout: 0
+            })
         }
+        p++
     } while (isNextPageAvailable)
 
     await browser.close()
